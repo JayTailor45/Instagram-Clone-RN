ab7d2dabac534f310f9a4ba8ad31241e91b48727
Jay Tailor <tailorj64@gmail.com>
2019-05-18 15:43:08 +0530

    Created Login UI

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,15 +1,45 @@
-import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, TouchableOpacity, Text, View, Image, ImageBackground} from 'react-native';
+
+import {Input, Item} from 'native-base'
 
 class Login extends Component {
 
   render() {
     return (
-        <View style={styles.container}>
-          <Text>
-            Login Screen
-          </Text>
-        </View>
+        <ImageBackground
+            style={[styles.container, {width: '100%', height: '100%'}]}
+            source={require('../assets/images/instagram-gradient-bg.png')}
+        >
+          <Image style={styles.instaLogo} source={require('../assets/images/instaLogoWhiteHD.png')}/>
+
+          <View style={{alignItems: 'center'}}>
+            <Item style={styles.inputContainer}>
+              <Input
+                  placeholder={'Email'}
+                  placeholderTextColor={'#fff'}
+                  style={{ color: '#fff'}}/>
+            </Item>
+            <Item style={styles.inputContainer}>
+              <Input
+                  secureTextEntry={true}
+                  placeholder={'Password'}
+                  placeholderTextColor={'#fff'}
+                  style={{ color: '#fff'}}/>
+            </Item>
+            <TouchableOpacity style={{ width: '90%' }}onPress={() => {}}>
+              <View style={styles.lgButtonContainer}>
+                <Text style={{color: '#fff'}}>Log In</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.signupContainer}>
+            <Text style={{color: '#fff'}}>Don't have an account?</Text>
+            <TouchableOpacity>
+              <Text style={{marginStart: 16, fontWeight: '600', color: '#fff'}}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
     )
   }
 }
@@ -17,9 +47,40 @@ class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  instaLogo: {
+    marginTop: 150,
+    alignSelf: 'center',
+    height: 60,
+    width: 200,
+    marginBottom: 50
+  },
+  inputContainer: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: '#ffffff10',
+    borderRadius: 5,
+    paddingStart: 16,
+    width: '90%',
+    marginTop: 16
+  },
+  lgButtonContainer: {
+    marginTop: 16,
+    borderColor: '#ffffff20',
+    borderRadius: 5,
+    padding: 16,
+    borderWidth: 1,
+    alignItems: 'center'
+  },
+  signupContainer: {
+    height: 80,
+    width: '100%',
+    bottom: 0,
+    position: 'absolute',
+    backgroundColor: '#ffffff20',
+    flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    justifyContent: 'center'
   }
 });
 
